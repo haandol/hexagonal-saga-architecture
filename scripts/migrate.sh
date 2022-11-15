620d5612fc1387101e5fc3a83436bc9f95ff6464
haandol <ldg55d@gmail.com>
2022-11-15 10:39:42 +0900

    make air and goose to use pre-built docker image

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -4,12 +4,10 @@ set -o allexport
 [ -f ./.env ] && . ./.env
 set +o allexport
 
-cd init
-
 echo $DB_USERNAME
 echo $DB_HOST:$DB_PORT
 export GOOSE_DBSTRING="$DB_USERNAME:$DB_PASSWORD@tcp($DB_HOST:$DB_PORT)/$DB_NAME?parseTime=true"
 
-echo $(goose mysql status)
+echo $(goose -dir init mysql status)
 
-exec goose mysql $1
+exec goose -dir init mysql $1
