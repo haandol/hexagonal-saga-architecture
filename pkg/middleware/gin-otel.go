9c096f382eb5f9465a8a07a73c9a31855bf777db
haandol <ldg55d@gmail.com>
2023-03-25 01:23:14 +0900

    disable healthy to otel

--- pkg/middleware/gin-otel.go
+++ pkg/middleware/gin-otel.go
@@ -1,10 +1,15 @@
 package middleware
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 )
 
 func OtelTracing(service string) gin.HandlerFunc {
-	return otelgin.Middleware(service)
+	return otelgin.Middleware(service, otelgin.WithFilter(func(r *http.Request) bool {
+		// Don't trace health checks
+		return r.URL.Path != "/healthy"
+	}))
 }
